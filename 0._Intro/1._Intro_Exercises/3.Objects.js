f25bac199f00aaf9e7020e73b8c2c77fc9676efd
abdulahi roble <abdulahi_2610@hotmail.com>
2021-09-07 13:40:56 +0200

    Done

--- 0._Intro/1._Intro_Exercises/3.Objects.js
+++ 0._Intro/1._Intro_Exercises/3.Objects.js
@@ -3,7 +3,14 @@
 // --------------------------------------
 // Exercise 1 - Retrieve value from object by key
 
-const myObj = {"message": "Hello, earthling! I bring peace."};
+const myObj = { "message": "Hello, earthling! I bring peace." };
+
+// const obj = Object.keys(myObj).forEach((key) => {
+//     console.log(myObj[key]);
+// });
+
+console.log(Object.values(myObj))
+
 
 // Log the message 
 
@@ -12,6 +19,12 @@ const myObj = {"message": "Hello, earthling! I bring peace."};
 
 // Create an object that has your name and age. 
 
+const object = {
+    name: "abdu",
+    age: 21
+}
+
+// console.log(object)
 
 // --------------------------------------
 // Exercise 3 - Add a property 
@@ -20,12 +33,22 @@ const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true
+
+// console.log(stackOverflow)
+
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
-const thisSong = {"description": "The best song in the world."}
+const thisSong = { "description": "The best song in the world." }
 
-// remove the property "description" and add a property called "about" that should say "Just a tribute." 
+// remove the property "description" and add a property called "about" that should say "Just a tribute." ¨
+
+delete thisSong.description
+
+thisSong.about = "just a tribute"
+
+console.log(thisSong)
 
 
 // --------------------------------------
--- 0._Intro/1._Intro_Exercises/4.Arrays.js
+++ 0._Intro/1._Intro_Exercises/4.Arrays.js
@@ -3,14 +3,20 @@
 // --------------------------------------
 // Exercise 1 - Array Positioning
 
-const letters = ["a","b","c"];
+const letters = ["a", "b", "c"];
 // show b in the console 
+
+// console.log(letters[1])
 
 
 // --------------------------------------
 // Exercise 2 - Array Positioning
 
-const friends = [];
+const object = { "name": "abdu" }
+
+const friends = [object];
+
+// console.log(friends)
 
 // What a lonely array. Add at least 3 friend objects to it.  
 
@@ -19,6 +25,8 @@ const friends = [];
 // Exercise 3 - Get the index of first occurance of that value. 
 
 const significantMathNumbers = [0, 2.718, 3.14159, 1729];
+
+// console.log(significantMathNumbers.indexOf(1729))
 
 // You want to programmatically find where the number 1729 is in the array.
 // programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
@@ -31,7 +39,9 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // You are a programmer. In one line (one statement) insert hamburger, soda and pizza between the elements cucumber and rocket
 
+diet.push("hamburger")
 
+// console.log(diet)
 
 
 // --------------------------------------
@@ -40,7 +50,9 @@ const diet = ["tomato", "cucumber", "rocket"];
 // Remove the LAST element of the array.
 // Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
 
+diet.pop()
 
+// console.log(diet)
 
 
 
@@ -49,26 +61,43 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
 
-
+const dinnerTray = diet;
 
 // --------------------------------------
 // Exercise 7 - For loop
 
-const lettersExpanded = ["a","b","c", "d", "e", "f", "g", "h"];
+const lettersExpanded = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 // log every second char in the array starting from b
+
+// var everySecondValue = function (array) {
+//     for (i = 1; i <= array.length - 1; i += 2) {
+//         console.log(array[i]);
+//     }
+// }
+
+
+// everySecondValue(lettersExpanded);
 
 
 
 // --------------------------------------
 // Exercise 8 - For loop and if statement
 
-const numbers  = [5, 3, 2, 7, 11, 12, 0, -20, 6];
+const numbers = [5, 3, 2, 7, 11, 12, 0, -20, 6];
 
-const discardedNumbers = [];
+const discardedNumber = [];
 
-// log the element if the number is above 6 or below 0
-// else push them to the array discardedNumbers
+const filter = numbers.map((number) => {
+    if (number < 0 || number > 6) {
+        // console.log(number)
+    } else {
+        // discardedNumber.push(number)
+
+        console.log(discardedNumber.push(number))
+    }
+})
+
 
 // --------------------------------------
 
